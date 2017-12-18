15e4475e80a56cabed64e245ddb9e2cf6e38636a
oscarfabiano <34660442+oscarfabiano@users.noreply.github.com>
2017-12-18 17:25:23 -0200

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,22 +1,23 @@
 /*
-Author  : Oscar Fabiano
-Version : 1.0 
-Javascript sample to create a 'diamond' with chars on console
-Goals : 1) How to work with a finobacci sequence
-        2) Array interation
-        3) Fun
+Author      : Oscar Fabiano
+Version     : 1.0 
+Description : Javascript sample to create a 'diamond' with chars on console
+              Goals : 1) How to work with a finobacci sequence
+                      2) Array interation
+                      3) Just 4Fun
+How to test : Copy & Past this code on https://jsfiddle.net/ and open the console to see the result        
 */
 
 var a = 0;
 var max = 100; //define max finobacci limit
-var finobacci = [1];
+var finobacci = [0];
 var char_space = ' '
 var char_diamond = '+'
 
 while (finobacci[finobacci.length-1] <= max) {
 
 	if (a > 1) {  
-  	finobacci.push(Number(finobacci[finobacci.length-1]) + Number(finobacci[finobacci.length-2])); //increment previous value with previous+1
+  	finobacci.push(Number(finobacci[finobacci.length-1]) + Number(finobacci[finobacci.length-2])); //increment previous + previous+2
   	
     if (finobacci.length>0) {
     	 if (finobacci[finobacci.length-1] < max) {
