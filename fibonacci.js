151e934af762326d531954cfd5b0045cd5a9ee15
Oscar Fabiano <34660442+oscarfabiano@users.noreply.github.com>
2019-01-20 19:37:51 -0500

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -2,7 +2,7 @@
 Author      : Oscar Fabiano
 Version     : 1.0 
 Description : Javascript sample to create a 'diamond' with chars on console
-              Objectives : 1) Show how to work with a fibonacci sequence
+              Objectives : 1) It shows how to work with a Fibonacci sequence
                            2) Working with array interation
                            3) Just 4Fun
 How to test : Copy & Past this code on https://jsfiddle.net/ and open the console to see the result        
